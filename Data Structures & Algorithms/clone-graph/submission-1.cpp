166bd1f9ce4114fb7ee69bf63b91ba02338f1b33
Karan Singh Bagga <karansingh9535@gmail.com>
2026-08-07 01:18:18 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,53 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* actualCloneGraph(Node* node, unordered_map<Node*, Node*> &clonedMap) {
+
+        if (node == nullptr) return node;
+        
+        if (clonedMap.find(node) != clonedMap.end()) {
+            return clonedMap[node];
+        }
+        
+        Node* cloneNode = new Node();
+        // store the reference right away!
+        clonedMap[node] = cloneNode;
+
+        vector<Node*> cloneNeighbors;
+
+        for ( auto n: node -> neighbors) {
+            cloneNeighbors.push_back( actualCloneGraph(n, clonedMap) );
+        }
+
+        cloneNode -> neighbors = cloneNeighbors;
+        cloneNode -> val = node -> val;
+
+        
+        return cloneNode;
+    }
+
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> clonedMap;
+        return actualCloneGraph(node, clonedMap);   
+    }
+};
